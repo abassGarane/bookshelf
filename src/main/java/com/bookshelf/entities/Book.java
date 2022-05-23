e026940aa1b57ad3ac551bc765a2d98d6dbe34c4
abassGarane <thelibertinewreck@gmail.com>
2022-05-23 08:53:10 +0300

    Created a onetoone relationship for books-rating

--- src/main/java/com/bookshelf/entities/Book.java
+++ src/main/java/com/bookshelf/entities/Book.java
@@ -52,4 +52,106 @@ public class Book {
     public Book() {
     }
 
+    public Book(String name, String bookPhotoUrl, String shortDesc, String synopsis, Integer numberOfPages, String isbn,
+            String language, Date dateOfPublishing, List<Authors> book_authors, Rating rating) {
+        this.name = name;
+        this.bookPhotoUrl = bookPhotoUrl;
+        this.shortDesc = shortDesc;
+        this.synopsis = synopsis;
+        this.numberOfPages = numberOfPages;
+        this.isbn = isbn;
+        this.language = language;
+        this.dateOfPublishing = dateOfPublishing;
+        this.book_authors = book_authors;
+        this.rating = rating;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getBookPhotoUrl() {
+        return bookPhotoUrl;
+    }
+
+    public void setBookPhotoUrl(String bookPhotoUrl) {
+        this.bookPhotoUrl = bookPhotoUrl;
+    }
+
+    public String getShortDesc() {
+        return shortDesc;
+    }
+
+    public void setShortDesc(String shortDesc) {
+        this.shortDesc = shortDesc;
+    }
+
+    public String getSynopsis() {
+        return synopsis;
+    }
+
+    public void setSynopsis(String synopsis) {
+        this.synopsis = synopsis;
+    }
+
+    public Integer getNumberOfPages() {
+        return numberOfPages;
+    }
+
+    public void setNumberOfPages(Integer numberOfPages) {
+        this.numberOfPages = numberOfPages;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public String getLanguage() {
+        return language;
+    }
+
+    public void setLanguage(String language) {
+        this.language = language;
+    }
+
+    public Date getDateOfPublishing() {
+        return dateOfPublishing;
+    }
+
+    public void setDateOfPublishing(Date dateOfPublishing) {
+        this.dateOfPublishing = dateOfPublishing;
+    }
+
+    public List<Authors> getBook_authors() {
+        return book_authors;
+    }
+
+    public void setBook_authors(List<Authors> book_authors) {
+        this.book_authors = book_authors;
+    }
+
+    public Rating getRating() {
+        return rating;
+    }
+
+    public void setRating(Rating rating) {
+        this.rating = rating;
+    }
+
 }
--- src/main/java/com/bookshelf/entities/Rating.java
+++ src/main/java/com/bookshelf/entities/Rating.java
@@ -1,9 +1,58 @@
 package com.bookshelf.entities;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
 
 @Entity
+Table(name="ratings")
 public class Rating {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
     private Integer rating;
-    private Long book_id;
+
+    @OneToOne(mappedBy = "ratings")
+    private Book book;
+
+    public Rating() {
+    }
+
+    public Rating(Integer rating, Book book) {
+        this.rating = rating;
+        this.book = book;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Integer getRating() {
+        return rating;
+    }
+
+    public void setRating(Integer rating) {
+        this.rating = rating;
+    }
+
+    public Book getBook() {
+        return book;
+    }
+
+    public void setBook(Book book) {
+        this.book = book;
+    }
+
+    @Override
+    public String toString() {
+        return "Rating [book=" + book + ", rating=" + rating + "]";
+    }
+    
+    
 }
